a30fd6a4c523c8c7b9606864b7233e7be8240847
DESKTOP-HU2QA0I\krisztian.vasari <krisztian.vasari@guidance.hu>
2023-05-08 09:31:32 +0200

    majdnem kész verzió

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,126 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, ChangeEvent} from 'react';
 import './App.css';
+import {DataConnection} from "./DataConnection";
+
 
 function App() {
 
+    const [todoList, setTodoList] = useState<Todo[]>([]);
+    const [loaded, setLoaded] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+    const [inputText, setInputText] = useState("");
+
+    const dataConnection = new DataConnection();
+
+    useEffect(() => {
+        dataConnection.getData().then((result: any) => {
+            setTodoList(result);
+            setLoaded(true);
+        }).catch((error: string) => {
+            alert(error)
+        })
+    }, []);
+
     let addNewTodoButtonText: string = 'Feladat hozzáadása';
+    let errorButtonText: string = 'Hiba!'
+    let addNewTodoOkButtonText: string = 'OK';
+
+    function addTodoClick() {
+        setOpenModal(true);
+    }
+
+    function closeModal() {
+        let newTodo: Todo = {
+            text: inputText,
+            complete: false
+        }
+        dataConnection.addData(newTodo);
+        const newTodoList = [...todoList, newTodo];
+        setTodoList(newTodoList);
+        setOpenModal(false);
+    }
+
+    const newTodoInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputText(e.target.value);
+    }
+
+    function errorClick() {
+        dataConnection.getData(true).then(result => {
+        }).catch(error => {
+            alert(error)
+        })
+    }
 
     return (
         <>
-            <div className="App">
-                <h2>Todo list</h2>
-                <CheckList />
-                <Button buttonText={addNewTodoButtonText} />
-            </div>
+            {!loaded ? <div>Töltődik...</div> :
+                <div className="App">
+                    <h2>Todo list</h2>
+                    <CheckList todoList={todoList}/>
+                    <Button buttonText={addNewTodoButtonText} onClick={addTodoClick}/>
+                    <Button buttonText={errorButtonText} onClick={errorClick}/>
+                    <Modal open={openModal} newTodoInputChange={newTodoInputChange}
+                           buttonFunction={closeModal} addNewTodoOkButtonText={addNewTodoOkButtonText}/>
+                </div>
+            }
         </>
     );
 }
 
-export function CheckList(): JSX.Element{
+export function CheckList(props: { todoList: Todo[] }): JSX.Element {
 
-    function handleCheckboxActiveState() {}
+    function handleCheckboxActiveState() {
+    }
 
     return (
         <>
-            {GetDataFromDb().map(todo => {
-                return (
-                    <>
-                        <input type="checkbox"
-                               checked={todo.active}
-                               onClick={() => handleCheckboxActiveState}
-                        />
-                        <p>{todo.text}</p>
-                    </>
-                )
-            })}
+            <table>
+                <tr>
+                    <th>Kész</th>
+                    <th>Feladat</th>
+                </tr>
+                {props.todoList.map(todo => {
+                    return (
+                        <>
+                            <tr>
+                                <td>
+                                    <input type="checkbox"
+                                           checked={todo.complete}
+                                           onClick={() => handleCheckboxActiveState}
+                                    />
+                                </td>
+                                <td>
+                                    <p>{todo.text}</p>
+                                </td>
+                            </tr>
+
+                        </>
+                    )
+                })}
+            </table>
         </>
     )
 }
 
-function GetDataFromDb(): Todo[] {
-
-    const [todoList, setTodoList] = useState<Todo[]>([]);
-
-    // todoList litát a DataConnection-ből kell feltölteni adattal, setTodoList
-    const todos: Todo[] = [
-        {
-            text: 'vásárlás',
-            active: true
-        },
-        {
-            text: 'mosogatás',
-            active: false
-        }
-    ]
+export function Button(props: { buttonText: string, onClick: any }): JSX.Element {
     return (
-        todos
+        <button onClick={props.onClick}>
+            {props.buttonText}
+        </button>
     )
 }
 
-export function Button(props: { buttonText: string }): JSX.Element {
+export function Modal(props: {open: boolean, newTodoInputChange: any, buttonFunction: any, addNewTodoOkButtonText: string}):JSX.Element {
+
+    if (!props.open) {
+        return <></>;
+    }
     return (
-        <button>
-            {props.buttonText}
-        </button>
+        <div>
+            <label>Új feladat: </label>
+            <input type='text' onChange={props.newTodoInputChange} />
+            <Button buttonText={props.addNewTodoOkButtonText} onClick={props.buttonFunction} />
+        </div>
     )
 }
 
@@ -69,5 +128,5 @@ export default App;
 
 export interface Todo {
     text: string;
-    active: boolean
+    complete: boolean
 }
--- src/DataConnection.tsx
+++ src/DataConnection.tsx
@@ -1,6 +1,38 @@
 import {Todo} from "./App";
 
-export default function DataConnection() {
+let counter: number = 0;
 
+export class DataConnection {
+
+    /*todos: Todo[] = [
+        {
+            text: 'vásárlás',
+            complete: true
+        },
+        {
+            text: 'mosogatás',
+            complete: false
+        },
+        {
+            text: 'főzés',
+            complete: true
+        }
+    ]*/
+    todos: Todo[] = [];
+
+    constructor() {}
+
+    async getData(isError: boolean = false) {
+        counter++;
+        if (counter % 10 === 0 || isError) {
+            throw new Error("Adatbázis kapcsolati hiba!")
+        }
+        return this.todos;
+    }
+
+    async addData(todo: Todo) {
+        this.todos.push(todo);
+        console.log(this.todos)
+    }
 
 }
